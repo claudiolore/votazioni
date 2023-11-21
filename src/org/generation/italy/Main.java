a90f51db878befc87071b7429804dca7b4469e70
claudiolore <claudio.lore001@gmail.com>
2023-11-21 12:29:52 +0100

    versione enzo

--- src/org/generation/italy/Main.java
+++ src/org/generation/italy/Main.java
@@ -1,61 +1,72 @@
-package org.generation.italy;
-
 import java.util.HashMap;
 import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-	String voto, risposta;
-	Scanner sc=new Scanner(System.in);
-	HashMap<String,Integer> candidati=new HashMap<>();
-		candidati.put("pippo",0);
-		candidati.put("pluto",0);
-		candidati.put("topolino",0);
-		candidati.put("paperino",0);
-	
-	System.out.println("Inserisci il tuo nome: ");
-	String nome=sc.nextLine();
-	nome.trim();									//.trim()
-	System.out.println("benvenuto "+ nome + " i candidati sono");
+		Scanner sc=new Scanner(System.in);
+		String scelta, risposta;
+		HashMap<String,Integer> Candidati=new HashMap<>() {
+			{
+				put("Mario Rossi",0);
+				put("Franco Gialli",0);
+				put("Pino Verdi",0);
+				put("Gino Marroni",0);
+				put("Lino Bianchi",0);
+		}};
 		
-	//stampa candidati		
-	System.out.println("Candidati disponibili:");
-	for(String cand:candidati.keySet())
-		System.out.println(cand);
-	
-	//votazione
-	
-	do {	
-		System.out.println("Chi vuoi votare?");
-		voto=sc.nextLine().trim();				//.trim() elimina spazi in eccesso
+		String messaggio="Grazie e arrivederci";
+		String inverted="";
 		
-		if(candidati.containsKey(voto))  			
-		{
-			candidati.put(voto, candidati.get(voto)+1);
-		}		
-		else 
-		{
-			System.out.println("Candidato non esistente");
-			risposta="si";
-		}
+		//inverto la stringa messaggio
+		for(int i=0;i<messaggio.length();i++)			//per ogni carattere della stringa messaggio
+			inverted+=messaggio.charAt(messaggio.length()-1-i);	
+		System.out.println(inverted);
 		
-		System.out.println("\nVuoi inserire un nuovo voto(si/no)?");
+		System.out.print("Inserisci il tuo nome: ");
+		String nome=sc.nextLine();
+		/*nome=	(nome.charAt(0)+"").toUpperCase()	//prende il primo carattere (nome.charAt(0)) e lo converte in stringa (+"")
+				+nome.substring(1);					//aggiunge la sottostringa a partire dalla posizione 1
+		*/
+		nome=nome.substring(0,1).toUpperCase()+nome.substring(1);
+		
+		System.out.println("Benvenuto "+nome+ ". I candidati sono:");
+		
+		System.out.println("Inserisci una chiave di ricerca per i candidati");
+		String ricerca=sc.nextLine();
+		for (String v:Candidati.keySet())
+			if (v.contains(ricerca))
+				System.out.println(v);
+		do {
+			System.out.println("Quale candidato scegli?");
+			scelta=sc.nextLine().trim();	//elimina gli spazi in eccesso all'inizio e alla fine della stringa	
+			if (Candidati.containsKey(scelta))    // se il nome inserito viene verificato, viene aggiunto all'HashMap
+				Candidati.put(scelta, Candidati.get(scelta)+1);
+			else 
+				System.out.println("Il candidato non esiste");
+			System.out.println("vuoi inserire un altro voto (s/n)?");
 			risposta=sc.nextLine();
-	}while(risposta.toLowerCase().equals("si"));		//.equalsIgnoreCase
-													
-	System.out.println("RISULTATI:");
-	for(String cand:candidati.keySet())
-		System.out.println(cand+" "+candidati.get(cand));
+				
+			} while (risposta.equalsIgnoreCase("s") || risposta.equalsIgnoreCase("sì") );		//risposta.toLowerCase().equals("s")
 	
-	sc.close();	
-	//.trim() elimina spazi in eccesso
-	//.toLowerCase()
-	//.toUpperCase()
-	//.equalsIgnoreCase()
-	//.startWith("x")  vero se la stringa inizia per x
-	
+		System.out.println("Risultato votazioni: ");
+		for (String v:Candidati.keySet())
+			System.out.println(v+"- "+Candidati.get(v));
+		
+		
+		
+		// .trim()					: rimuove gli spazi in eccesso all'inizio e alla fine
+		// .toLowerCase()			: converte in minuscolo
+		// .toUpperCase()			: converte in maiuscolo
+		// .startsWith("x")			: restituisce vero se la stringa inizia per "x" 
+		// .equalsIgnoreCase(s1)	: restituisce vero se la stringa è uguale alla stringa s1 (ignorando maiuscole/minuscole)
+		// .contains(s1)			: restituisce vero se la stringa contiene s1
+		// .charAt(pos)				: restituisce il singolo carattere nella posizione pos
+		// .subString(pos)			: restituisce la sottostringa a partire dalla posizione pos
+		// .subString(pos1,pos2)	: restituisce la sottostringa a partire dalla posizione pos1 fino alla pos2 (esclusa)
+		
+		
+
 	}
-	
+
 }
